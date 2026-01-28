279572d4659fedde7e8d3715d2a739c55bc5f19b
Max-Dubois <duboismax62153@gmail.com>
2026-01-28 17:22:35 +0100

    a propos

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
 
+@app.route('/about')
+def about():
+    return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
+
 if __name__ == '__main__':
     app.run(debug=True)
