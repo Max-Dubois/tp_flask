993391bf07bdc72e7fb8c3a04dd00565a6e92893
Max-Dubois <duboismax62153@gmail.com>
2026-02-11 13:58:45 +0100

    page web flask

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bienvenue sur ma page d'accueil !"
+    return render_template('accueil.html')
 
-@app.route('/about')
+@app.route('/carte')
 def about():
-    return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
+    return render_template('carte.html')
 
 # @app.route('/about')
 # def about():
 #     return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
