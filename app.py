64fbf8fb33c9b339b517bc81c64628a7653513dc
Max-Dubois <duboismax62153@gmail.com>
2026-02-11 16:42:52 +0100

    ajout kmeans

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import folium
 from flask import Flask, render_template, request, send_from_directory
 import os
 import tkinter as tk
+import cv2
+import numpy as np
+import io
+from flask import send_file
+from sklearn.cluster import KMeans
 
 app = Flask(__name__)
 
@@ -42,6 +47,28 @@ def serve_image(full_path):
     directory = os.path.dirname(full_path)
     filename = os.path.basename(full_path)
     return send_from_directory(directory, filename)
+
+@app.route('/kmeans', methods=['POST'])
+def kmeans():
+    image_path = request.form.get('image_path')
+    k_clusters = int(request.form.get('k', 5))
+
+    img = cv2.imread(image_path)
+    if img is None:
+        return "Erreur : Impossible de charger l'image.", 400
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    pixels = img_rgb.reshape((-1, 3))
+
+    kmeans = KMeans(n_clusters=k_clusters, init="k-means++", n_init=10, max_iter=100, random_state=42)
+    kmeans.fit(pixels)
+
+    couleurs_centres = np.uint8(kmeans.cluster_centers_)
+    pixels_segmentes = couleurs_centres[kmeans.labels_]
+    image_finale = pixels_segmentes.reshape(img_rgb.shape)
+
+    _, buffer = cv2.imencode('.jpg', cv2.cvtColor(image_finale, cv2.COLOR_RGB2BGR))
+    return send_file(io.BytesIO(buffer), mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
