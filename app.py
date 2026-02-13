afe0dd60b95e90e2d41e697861db43bfeb518a1c
Max-Dubois <duboismax62153@gmail.com>
2026-02-13 09:15:12 +0100

    test approche hierarchique

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ import cv2
 import numpy as np
 import io
 from sklearn.cluster import KMeans
+from sklearn import metrics
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.cluster import AgglomerativeClustering
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -79,6 +85,48 @@ def generate_kmeans_image():
     labels_full = kmeans.predict(data_full)
 
     couleurs_centres = np.uint8(kmeans.cluster_centers_[:, :3])
+    image_finale = couleurs_centres[labels_full].reshape(img_rgb.shape)
+
+    _, buffer = cv2.imencode('.jpg', cv2.cvtColor(image_finale, cv2.COLOR_RGB2BGR))
+    return send_file(io.BytesIO(buffer), mimetype='image/jpeg')
+
+
+@app.route('/hierarchique', methods=['POST'])
+def approcheHierarchique():
+    image_path = request.form.get('image_path')
+    k = request.form.get('k', 5)
+    return render_template('resultH.html', image_path=image_path, k=k)
+
+@app.route('/hierarchical_image')
+def generate_hierarchical_image():
+    image_path = request.args.get('image_path')
+    k_clusters = int(request.args.get('k', 5))
+
+    img = cv2.imread(image_path)
+    if img is None: return "Erreur", 400
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    h, w, _ = img_rgb.shape
+    yy, xx = np.meshgrid(np.linspace(0, 255, h), np.linspace(0, 255, w), indexing='ij')
+    
+    data_full = np.dstack((img_rgb, xx, yy)).reshape((-1, 5))
+
+    scale_percent = 30 
+    img_small = cv2.resize(img_rgb, (int(w*scale_percent/100), int(h*scale_percent/100)), interpolation=cv2.INTER_AREA)
+    sh, sw, _ = img_small.shape
+    s_yy, s_xx = np.meshgrid(np.linspace(0, 255, sh), np.linspace(0, 255, sw), indexing='ij')
+    data_small = np.dstack((img_small, s_xx, s_yy)).reshape((-1, 5))
+
+    hierarchique = AgglomerativeClustering(n_clusters=k_clusters)
+    labels_small = hierarchique.fit_predict(data_small)
+
+    couleurs_centres = np.zeros((k_clusters, 3), dtype=np.uint8)
+    for i in range(k_clusters):
+        mask = (labels_small == i)
+        if np.sum(mask) > 0:
+            couleurs_centres[i] = np.mean(data_small[mask][:,:3], axis=0).astype(np.uint8)
+
+    labels_full = hierarchique.fit_predict(data_full)
     image_finale = couleurs_centres[labels_full].reshape(img_rgb.shape)
 
     _, buffer = cv2.imencode('.jpg', cv2.cvtColor(image_finale, cv2.COLOR_RGB2BGR))
